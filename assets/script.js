ef98a072d028ccc6297376866f3e4208868d1981
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-10 14:55:01 -0600

    added a clear button to start over and remove local storage

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ $("#currentDay").text(moment().format('MMMM Do YYYY HH:mm:ss'))
     setInterval(timeOfDay, 1000)
 };
 
+
 // // pseudo code:
 // 1. change colors of container based on time of day 
 // 2. save information to local storage 
@@ -102,11 +103,46 @@ setInterval(changeColor, 1000)
 // saving textarea input to local storage
 $(".saveBtn").on("click", function() {
     let input = $(this).siblings(".description").val();
-    let time = $(this).siblings(".hour").text();
+    let time = $(this).parent().attr("id");
     localStorage.setItem(time, input)
 })
 
 
+
+// pull users input from local storage
+let userInput9 = localStorage.getItem("time-9");
+$("#9text").text(userInput9);
+
+let userInput10 = localStorage.getItem("time-10");
+$("#10text").text(userInput10);
+
+let userInput11 = localStorage.getItem("time-11");
+$("#11text").text(userInput11);
+
+let userInput12 = localStorage.getItem("time-12");
+$("#12text").text(userInput12);
+
+let userInput13 = localStorage.getItem("time-13");
+$("#13text").text(userInput13);
+
+let userInput14 = localStorage.getItem("time-14");
+$("#14text").text(userInput14);
+
+let userInput15 = localStorage.getItem("time-15");
+$("#15text").text(userInput15);
+
+let userInput16 = localStorage.getItem("time-16");
+$("#16text").text(userInput16);
+
+let userInput17 = localStorage.getItem("time-17");
+$("#17text").text(userInput17);
+
+
+// clear day planner button
+$("#clearBtn").click(function(){
+    $("#9text, #10text, #11text, #12text, #13text, #14text, #15text, #16text, #17text").val("");
+    localStorage.clear();
+});
 
 
 

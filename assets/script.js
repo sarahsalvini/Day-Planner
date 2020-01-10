8650921ec2456f6cc4e62a2f565665a5e859b99c
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-10 11:10:46 -0600

    added each hour element

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var date = moment().format('MMMM Do YYYY')
+
+// displaying date on heading
+$("#currentDay").text(date);
+    console.log(date);
+
+
+// create time blocks for every hour between 9 & 5
+
+
+
+
+// connect time to each block to be able to show past, present, future
+
+// show colors for each time block
+
+// be able to save content to local storage 
+
